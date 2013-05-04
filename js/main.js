e1487d7da828b6e0507b33de1f5e36655cd58959
f03lipe <pires.a.felipe@gmail.com>
2013-05-04 04:54:13 -0300

    2 hours spent on the share buttons

--- js/main.js
+++ js/main.js
@@ -114,3 +114,26 @@ $(document).scroll(function() {
 	}
 	
 })
+
+$('#twitter').sharrre({
+	share: { twitter: true },
+	template: '<a class="box" href="#"><div class="share"><span></span>Tweet</div></a>',
+	enableHover: false,
+	enableTracking: true,
+	buttons: { twitter: {via: '_JulienH'}},
+	click: function(api, options){
+		api.simulateClick();
+		api.openPopup('twitter');
+	}
+});
+
+$('#facebook').sharrre({
+	share: { facebook: true },
+	template: '<a class="box" href="#"><div class="share"><span></span>Like</div></a>',
+	enableHover: false,
+	enableTracking: true,
+	click: function(api, options){
+		api.simulateClick();
+		api.openPopup('facebook');
+	}
+});
